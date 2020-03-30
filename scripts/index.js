34cbe3f496e04241e6f729f69b19756b1ce93795
dai00040 <59571015+dai00040@users.noreply.github.com>
2020-03-30 14:58:38 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,5 @@
-// Load your images on page-load
-function preloader() {
-    const imagesPaths = [
-        "./images/img-1.jpg",
-        "./images/img-2.jpg",
-        "./images/img-3.jpg"
-];
-    const images = [];
-    for (let i = 0; i < imagesPaths.length; i++) {
-        images[i] = new Image();
-        images[i].src = imagesPaths[i];
-    }
-
-    // Images ready to be used:
-    console.log(`Preloaded images:\n\t${images[0].src}\n\t${images[1].src}\n\t${images[2].src}`);
-};
-window.addEventListener("load", preloader);
-
-// creating shortcut for printing
 const p = console.log;
 
-// get the references to interactive HTML elements
 const ctrlBtns = document.querySelectorAll('.controls button');
 const cntnr = document.querySelector('.container');
 let btnsArray = [
@@ -28,7 +8,6 @@ let btnsArray = [
     ctrlBtns[2]
 ];
 
-// create array that will store the data
 let pages = [
     {
         heading: "Water Energy",
@@ -46,32 +25,19 @@ let pages = [
         thumb: "./images/img-3.jpg"
     }
 ];
-// create function that handles click-event
+
 function clickBtn(ev) {
-
-    // Brightspsce Ref: Week 6
     let clickedButton = ev.target;
-
-    // MDN Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
     let index = btnsArray.indexOf(clickedButton);
     p(index);
-
-    // MDN Ref: https://developer.mozilla.org/en-US/docs/Web/API/Element
     p(clickedButton.id);
-    // handle moving id-active to the currently clicked button 
 
-    // remove currently present id="active"
     for (let i = 0; i < ctrlBtns.length; i++) {
-        // claiming that current element in the loop containe attribute
         if (ctrlBtns[i].id) {
-            // MDN ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/removeAttribute
             ctrlBtns[i].removeAttribute('id');
         }
     }
-
-
     clickedButton.id = 'active';
-
 
     cntnr.innerHTML = `
         <h2>${pages[index].heading}</h2>
@@ -81,7 +47,7 @@ function clickBtn(ev) {
         <p class="bodyText">${pages[index].bodyText}</p>
     `;
 }
-// register your buttons for click event
+
 for (let i = 0; i < ctrlBtns.length; i++) {
     ctrlBtns[i].addEventListener('click', clickBtn);
 }
